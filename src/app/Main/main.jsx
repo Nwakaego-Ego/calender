5720c8f350d68e123ccea4d36187a7e74ff7aa14
Ego Nwaekpe <glorynwaekpe@gmail.com>
2023-12-30 08:52:27 -0800

    working on the dates

--- src/app/Main/main.jsx
+++ src/app/Main/main.jsx
@@ -1,12 +1,10 @@
 import React from "react";
 import "./main.css";
 
-const Main = () => {
+const Main = ({ value, onChange }) => {
+  console.log(value);
   const weekDays = ["Sat", "Sun", "Mon", "Tue", "Wed", "Thur", "Fri"];
 
-  //   if (weekDays === "fri") {
-  //     const friday = document.createElement("fri");
-  //   }
   return (
     <>
       <div
@@ -23,7 +21,7 @@ const Main = () => {
           return (
             <div
               key={id}
-              className={`border border-r-0 border-t-0 p-3 ${
+              className={`border border-r-0 border-t-0 p-3 font-bold ${
                 day === "Fri" ? "friday" : ""
               }`}
             >
@@ -32,37 +30,37 @@ const Main = () => {
           );
         })}
 
-        <div className={1}>1</div>
-        <div className={2}>2</div>
-        <div className={3}>3</div>
-        <div className={4}>4</div>
-        <div className={5}>5</div>
-        <div className={6}>6</div>
-        <div className={7}>7</div>
-        <div className={8}>8</div>
-        <div className={9}>9</div>
-        <div className={10}>10</div>
-        <div className={11}>11</div>
-        <div className={12}>12</div>
-        <div className={13}>13</div>
-        <div className={14}>14</div>
-        <div className={15}>15</div>
-        <div className={16}>16</div>
-        <div className={17}>17</div>
-        <div className={18}>18</div>
-        <div className={19}>19</div>
-        <div className={20}>20</div>
-        <div className={21}>21</div>
-        <div className={22}>22</div>
-        <div className={23}>23</div>
-        <div className={24}>24</div>
-        <div className={25}>25</div>
-        <div className={26}>26</div>
-        <div className={27}>27</div>
-        <div className={28}>28</div>
-        <div className={29}>29</div>
-        <div className={30}>30</div>
-        <div className={31}>31</div>
+        <div className="border p-3">1</div>
+        <div className="border border-l-0 p-3 border-t-0">2</div>
+        <div className="border border-l-0 p-3 border-t-0">3</div>
+        <div className="border border-l-0 p-3 border-t-0">4</div>
+        <div className="border border-l-0 p-3 border-t-0">5</div>
+        <div className="border border-l-0 p-3 border-t-0">6</div>
+        <div className="border border-l-0 p-3 border-t-0 ">7</div>
+        <div className="border border-t-0 p-3">8</div>
+        <div className="border border-l-0 border-t-0 p-3">9</div>
+        <div className="border border-l-0 border-t-0 p-3">10</div>
+        <div className="border border-l-0 border-t-0 p-3">11</div>
+        <div className="border border-l-0 border-t-0 p-3">12</div>
+        <div className="border border-l-0 border-t-0 p-3">13</div>
+        <div className="border border-l-0 border-t-0 p-3">14</div>
+        <div className="border border-t-0 p-3">15</div>
+        <div className="border border-l-0 border-t-0 p-3">16</div>
+        <div className="border border-l-0 border-t-0 p-3">17</div>
+        <div className="border border-l-0 border-t-0 p-3">18</div>
+        <div className="border border-l-0 border-t-0 p-3">19</div>
+        <div className="border border-l-0 border-t-0 p-3">20</div>
+        <div className="border border-l-0 border-t-0 p-3">21</div>
+        <div className="border border-t-0 p-3">22</div>
+        <div className="border border-l-0 border-t-0 p-3">23</div>
+        <div className="border border-l-0 border-t-0 p-3">24</div>
+        <div className="border border-l-0 border-t-0 p-3">25</div>
+        <div className="border border-l-0 border-t-0 p-3">26</div>
+        <div className="border border-l-0 border-t-0 p-3">27</div>
+        <div className="border border-l-0 border-t-0 p-3">28</div>
+        <div className="border border-t-0 p-3">29</div>
+        <div className="border border-l-0 border-t-0 p-3">30</div>
+        <div className="border border-l-0 border-t-0 p-3">31</div>
       </div>
     </>
   );
--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,11 @@
+import React, { useState } from "react";
 import Main from "../app/Main/main";
 
 export default function Home() {
+  const [currentDate, setCurrentDate] = useState(new Date());
   return (
     <main className="">
-      <Main />
+      <Main value={currentDate} onChange={setCurrentDate} />
     </main>
   );
 }
