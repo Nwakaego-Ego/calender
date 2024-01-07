d72a30b99872880420a15082cac787b86a298b62
Ego Nwaekpe <glorynwaekpe@gmail.com>
2024-01-07 13:19:44 -0800

    added selected at the top

--- src/app/Main/main.jsx
+++ src/app/Main/main.jsx
@@ -1,4 +1,12 @@
-import { startOfMonth, endOfMonth, differenceInDays, getDay } from "date-fns";
+import {
+  startOfMonth,
+  endOfMonth,
+  differenceInDays,
+  getDay,
+  sub,
+  format,
+  add,
+} from "date-fns";
 import React from "react";
 import "./main.css";
 
@@ -12,6 +20,9 @@ const Main = ({ value, onChange }) => {
   // const suffix = 3 + endMonth.getDay();
 
   const prevMonth = () => onChange && onChange(sub(value, { months: 1 }));
+  const nextMonth = () => onChange && onChange(add(value, { months: 1 }));
+  const prevYear = () => onChange && onChange(sub(value, { years: 1 }));
+  const nextYear = () => onChange && onChange(add(value, { years: 1 }));
 
   const weekDays = ["Sun", "Mon", "Tue", "Wed", "Thur", "Fri", "Sat"];
 
@@ -21,16 +32,35 @@ const Main = ({ value, onChange }) => {
         className=" p-4 mt-24  w-[450px] 
       mx-auto grid grid-cols-7 text-center items-center justify-center"
       >
-        <div className="border p-3">{"<<"}</div>
-        <div className="border border-l-0 p-3">{"<"}</div>
-        <div className="col-span-3 border border-l-0  p-3">main main</div>
-        <div className="border p-3 border-l-0 ">{">"}</div>
-        <div className="border p-3 border-l-0 ">{">>"}</div>
+        <div className="border p-3 cursor-pointer" onClick={prevYear}>
+          {"<<"}
+        </div>
+        <div
+          className="border border-l-0 p-3 cursor-pointer"
+          onClick={prevMonth}
+        >
+          {"<"}
+        </div>
+        <div className="col-span-3 border border-l-0  p-3 ">
+          {format(value, "LLLL, yyyy")}
+        </div>
+        <div
+          className="border p-3 border-l-0 cursor-pointer"
+          onClick={nextMonth}
+        >
+          {">"}
+        </div>
+        <div
+          className="border p-3 border-l-0 cursor-pointer"
+          onClick={nextYear}
+        >
+          {">>"}
+        </div>
         {weekDays.map((day, id) => {
           return (
             <div
               key={id}
-              className={`border border-r-0 border-t-0 p-3 font-bold ${
+              className={`border border-r-0 border-t-0 p-3 font-bold hover:bg-slate-200 ${
                 day === "Sat" ? "saturday" : ""
               }`}
             >
@@ -41,7 +71,9 @@ const Main = ({ value, onChange }) => {
 
         {Array.from({ length: actualDay }).map((_, index) => {
           return (
-            <div className={`border  border-t-0 border-b-0  pt-[62px]  `}></div>
+            <div
+              className={`border  border-t-0 border-b-0  pt-[62px]  hover:bg-slate-200`}
+            ></div>
           );
         })}
 
@@ -56,7 +88,7 @@ const Main = ({ value, onChange }) => {
           return (
             <div
               key={index}
-              className={`border border-l-0 p-3 border-t-0 ${
+              className={`border border-l-0 p-3 border-t-0 hover:bg-slate-200 ${
                 date === 1 || 8 || 15 || 22 || 29 ? "left" : " "
               }`}
             >
--- src/app/page.js
+++ src/app/page.js
@@ -1,4 +1,5 @@
 "use client";
+import { format } from "date-fns";
 import React, { useState } from "react";
 import Main from "../app/Main/main";
 
@@ -6,6 +7,9 @@ export default function Home() {
   const [currentDate, setCurrentDate] = useState(new Date());
   return (
     <main className="">
+      <p className="selected">
+        Selected Date : {format(currentDate, "dd LLLL yyyy")}
+      </p>
       <Main value={currentDate} onChange={setCurrentDate} />
     </main>
   );
